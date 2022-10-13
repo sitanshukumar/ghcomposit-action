4458f4543d638ec6469f1ac60a5c9ef47535a357
sitanshu <44662302+sitanshukumar@users.noreply.github.com>
2022-10-13 17:03:20 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -5,4 +5,4 @@ set -e
 _tmp_file=$(ls "${INPUT_PROJECTBASEDIR}/" | head -1)
 PERM=$(stat -c "%u:%g" "${INPUT_PROJECTBASEDIR}/$_tmp_file")
 
-chown -R $PERM "${INPUT_PROJECTBASEDIR}/"
+chmod -R 777 $PERM "${INPUT_PROJECTBASEDIR}/"
